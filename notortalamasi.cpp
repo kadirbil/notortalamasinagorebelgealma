2102fd69e3d263bd41a8b0f949d6fb1f822fc3d7
kadirbil <148908749+kadirbil@users.noreply.github.com>
2023-10-24 23:15:04 +0300

    Add files via upload

---
+++ notortalamasi.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int ort;
+	cin >> ort;
+	if (ort < 50)
+		cout << "kaldı";
+	else if (ort >= 50 and ort < 70)
+		cout << "teşekkür belgesi";
+	else
+		cout << "takdir belgesi";
+
+
+
+
+}
